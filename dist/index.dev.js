05b386320045b8e616fc2e1ba2aa5d337f9b225d
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2023-09-23 12:32:54 +0300

    Completed Assignment

---
+++ dist/index.dev.js
@@ -0,0 +1,41 @@
+"use strict";
+
+function shout(inputString) {
+  var upperCase = inputString.toUpperCase();
+  return upperCase;
+}
+
+console.log(shout('hello'));
+
+function whisper(inputString) {
+  var lowerCase = inputString.toLowerCase();
+  return lowerCase;
+}
+
+console.log(whisper('HELLO'));
+
+function logShout(inputString) {
+  var upperCaseString = inputString.toUpperCase();
+  console.log(upperCaseString);
+}
+
+logShout('hello');
+
+function logWhisper(inputString) {
+  var lowerCaseString = inputString.toLowerCase();
+  console.log(lowerCaseString);
+}
+
+logWhisper('HELLO');
+
+function sayHiToHeadphonedRoommate(string) {
+  if (string === string.toLowerCase()) {
+    return "I can\'t hear you!";
+  } else if (string === string.toUpperCase()) {
+    return "YES INDEED!";
+  } else if (string === "Let's have dinner together!") {
+    return "I would love to!";
+  } else {
+    return "What did you say?";
+  }
+}
